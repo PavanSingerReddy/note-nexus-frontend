066b582441f9254bf15d6be16b55d6e5de554aa6
corsproxy123 <115952043+corsproxy123@users.noreply.github.com>
2023-11-17 18:31:46 +0530

    add debounce search using backend api

--- src/context/NotesContextProvider.jsx
+++ src/context/NotesContextProvider.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import NotesContext from './NotesContext'
+import httpRequestAxiosQueueUtility from '../utils/HttpRequestAxiosQueueUtility'
 
 // Notes Context provider holds all the states of the Notes and we can import this states using the Notes context
-const NotesContextProvider = ({children}) => {
+const NotesContextProvider = ({ children }) => {
 
   // notes state holds all the notes of the user
   const [notes, setNotes] = useState([])
@@ -12,28 +13,80 @@ const NotesContextProvider = ({children}) => {
   const [searchTerm, setSearchTerm] = useState("")
   // progress bar holds the state of the progress bar whether the progress bar should be shown using the show variable and the progress percentage using width variable
   const [progressBar, setProgressBar] = useState({
-    show:true,
-    width:0
+    show: true,
+    width: 0
   })
 
-  // filteredNotes which filters the notes state based on the search term and stores them in the filteredNotes variable which changes dynamically when the notes state changes as the filtered notes variable depends on the notes state and searchTerm state
-  const filteredNotes = searchTerm.trim() === "" ?notes:notes.filter((note)=>(
-    // filteredNotes variable checks both the note title and note context for searching with the search term
-    note.title.toLowerCase().includes(searchTerm.toLowerCase()) || note.content.toLowerCase().includes(searchTerm.toLowerCase())
-  ))
+  const [filteredNotes, setFilteredNotes] = useState([]);
+
+  // Debounce Function: The purpose of the debounce function is to limit the number of times a particular function can be called. It does this by introducing a delay. If the function is called again before the delay has passed, the timer is reset. This is useful in our case because we don’t want to call the search API every time the user presses a key. Instead, we want to wait until the user has stopped typing.
+
+  // below debounce version is a more generalized way of writing debounce function it can be used in any other code base not only on this current code base
+  // Define a class named Debouncer
+  class Debouncer {
+    // The constructor method is a special method for creating and initializing an object created with a class.
+    // It takes two parameters: a function to debounce and a delay in milliseconds.
+    constructor(func, delay) {
+      // Store the function and delay as properties of the object.
+      this.func = func;
+      this.delay = delay;
+      // Initialize a property for the debounce timer. This will be used to hold the timeout ID of the debounced function.
+      this.debounceTimer = null;
+    }
+
+    // Define a method named debounced that takes any number of arguments.
+    // This method will be used to call the debounced function.
+    debounced(...args) {
+      // Clear any existing timeout. This cancels any previous calls to the debounced function that haven't executed yet.
+      clearTimeout(this.debounceTimer);
+      // Return a new promise. This allows the caller to wait for the debounced function to execute.
+      return new Promise((resolve) => {
+        // Set a timeout to call the debounced function after the specified delay.
+        // When the debounced function is called, resolve the promise with its return value.
+        // The spread syntax (...) is used to pass all arguments to the function.
+        this.debounceTimer = setTimeout(() => resolve(this.func(...args)), this.delay);
+      });
+    }
+
+    // Define a method named cancel to cancel any ongoing debounce.
+    cancel() {
+      // Clear the timeout. This cancels any calls to the debounced function that haven't executed yet.
+      clearTimeout(this.debounceTimer);
+    }
+  }
+
+
+
+
+
+  // Search Function: This function is used to call your search API. It takes a search term as an argument, makes a request to your API, and returns the result.
+  const searchNotes = async (searchTerm) => {
+    const response = await httpRequestAxiosQueueUtility.authenticatedGet(`${import.meta.env.VITE_SEARCH_URL}${searchTerm}`);
+    const data = await response.data
+    return data;
+  }
+
 
   // This setSortedNotes is used to sort the notes based on the note updated date from newest note to the starting to the oldest note to the ending
-  const setSortedNotes = (notes)=>{
+  const setSortedNotes = (notes) => {
     // creates a new array because sort() function mutates the original array. sort() function then sorts the array using the comparator function in descending order 
-    const sortedNotes = [...notes].sort((a,b)=> new Date(b.updatedAt) - new Date(a.updatedAt))
+    const sortedNotes = [...notes].sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
     // set's the new sorted notes as the new notes state
     setNotes(sortedNotes);
   }
 
+  // This setSortedFilteredNotes is used to sort the notes based on the note updated date from newest note to the starting to the oldest note to the ending
+  const setSortedFilteredNotes = (notes) => {
+    // creates a new array because sort() function mutates the original array. sort() function then sorts the array using the comparator function in descending order 
+    const sortedNotes = [...notes].sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
+    // set's the new sorted notes as the new filteredNotes state
+    setFilteredNotes(sortedNotes);
+  }
+
   return (
     // The values we provide in the Notes context provider will be accessible by the child components of the Notes context provider 
-    <NotesContext.Provider value={{notes,searchTerm,setSearchTerm,filteredNotes,setSortedNotes,showNoteDeleteModal,setShowNoteDeleteModal,progressBar,setProgressBar}} >
-        {children}
+    <NotesContext.Provider value={{ notes, searchTerm, setSearchTerm, filteredNotes, setSortedNotes, setSortedFilteredNotes, showNoteDeleteModal, setShowNoteDeleteModal, progressBar, setProgressBar, Debouncer, searchNotes }} >
+      {children}
     </NotesContext.Provider>
   )
 }
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Note from './Note'
 import { useNavigate } from 'react-router-dom'
 import httpRequestAxiosQueueUtility from '../utils/HttpRequestAxiosQueueUtility';
@@ -11,7 +11,7 @@ const Notes = () => {
   const navigate = useNavigate();
 
   // used for getting the states from the notes context 
-  const { filteredNotes, setSortedNotes, showNoteDeleteModal, setShowNoteDeleteModal } = useContext(NotesContext)
+  const { filteredNotes, notes, setSortedNotes, setSortedFilteredNotes, Debouncer, searchNotes, showNoteDeleteModal, setShowNoteDeleteModal, searchTerm } = useContext(NotesContext)
 
   // setting the state of the selectedNote
   const [selectedNote, setSelectedNote] = useState(null);
@@ -64,6 +64,31 @@ const Notes = () => {
     //async function which fetches the user notes 
     fetchData();
   }, [])
+
+
+  // Debounced Search Function: Here we create a debounced version of our search function using the Debouncer class we defined in the NotesContext. This new function will wait for 300 milliseconds after the last call before executing. If it is called again within this time, it will reset the timer.
+  // Create a ref object that holds an instance of the Debouncer class.
+  // The Debouncer class is initialized with the searchNotes function and a delay of 300 milliseconds.
+  // useRef ensures that the same Debouncer instance is used across all renders of the component.
+  const debouncedSearchNotes = useRef(new Debouncer(searchNotes, 300));
+
+  // useEffect is a React hook that runs side-effects in function components.
+  // The function passed to useEffect will run after the render is committed to the screen.
+  // The second argument is an array of dependencies for this effect. 
+  // The effect will only run when one of these dependencies has changed since the last render.
+  useEffect(() => {
+    // Check if the search term is empty.
+    // If it is, cancel any ongoing debounced search and set the sorted and filtered notes to the original notes.
+    if (searchTerm.trim() === "") {
+      debouncedSearchNotes.current.cancel();
+      setSortedFilteredNotes(notes);
+    } else {
+      // If the search term is not empty, start a debounced search.
+      // debouncedSearchNotes.current.debounced(searchTerm) returns a promise that resolves with the search results.
+      // When the promise resolves, set the sorted and filtered notes to the search results.
+      debouncedSearchNotes.current.debounced(searchTerm).then(data => setSortedFilteredNotes(data));
+    }
+  }, [searchTerm, notes]); // The effect depends on searchTerm and notes. It will run again whenever one of these values changes.
 
 
 
